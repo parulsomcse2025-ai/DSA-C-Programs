eda3622093317b8abe6d3a99046a3e5abe249713
Parul Som <somp4407@gmail.com>
2026-08-23 21:32:20 +0530

    Add 2D matrix addition program

---
+++ 2d_matrix_addition.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+void inputMatrix(int matrix[10][10], int rows, int cols)
+{
+    printf("Enter elements of matrix:\n");
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+}
+
+void addMatrices(int matrix1[10][10], int matrix2[10][10],
+                 int result[10][10], int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            result[i][j] = matrix1[i][j] + matrix2[i][j];
+        }
+    }
+}
+
+void displayMatrix(int matrix[10][10], int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+        {
+            printf("%d\t", matrix[i][j]);
+        }
+
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int matrix1[10][10], matrix2[10][10], result[10][10];
+    int rows, cols;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &cols);
+
+    printf("\nFirst Matrix\n");
+    inputMatrix(matrix1, rows, cols);
+
+    printf("\nSecond Matrix\n");
+    inputMatrix(matrix2, rows, cols);
+
+    addMatrices(matrix1, matrix2, result, rows, cols);
+
+    printf("\nFirst Matrix:\n");
+    displayMatrix(matrix1, rows, cols);
+
+    printf("\nSecond Matrix:\n");
+    displayMatrix(matrix2, rows, cols);
+
+    printf("\nResultant Matrix (Addition):\n");
+    displayMatrix(result, rows, cols);
+
+    return 0;
+}
