fbf48ae1dca0fb2fd1e85e4f17d4e7fea4bed429
Parul Som <somp4407@gmail.com>
2026-08-23 22:33:41 +0530

    Add bubble sort

---
+++ bubble_sort.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+void inputArray(int arr[], int n)
+{
+    printf("Enter %d elements:\n", n);
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+}
+
+void bubbleSort(int arr[], int n)
+{
+    int temp;
+
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void displayArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    printf("\n");
+}
+
+int main()
+{
+    int arr[100];
+    int n;
+
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    inputArray(arr, n);
+
+    printf("\nOriginal Array: ");
+    displayArray(arr, n);
+
+    bubbleSort(arr, n);
+
+    printf("Sorted Array: ");
+    displayArray(arr, n);
+
+    return 0;
+}
